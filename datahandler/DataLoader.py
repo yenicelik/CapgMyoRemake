1f0eb33248bef54a684b27fb24ada629dfc255c7
yenicelik <segaslp@gmail.com>
2017-04-18 20:17:30 +0200

    Starting to implement a cross-session/cross-subject server

---
+++ datahandler/DataLoader.py
@@ -0,0 +1,21 @@
+from __future__ import print_function
+import numpy as np
+from Importer import *
+
+class DataLoader(object):
+    """ This function is here to load any kind of
+    """
+
+    def __init__(self, super_matrix):
+        self.super_matrix = super_matrix
+
+
+
+if __name__ == '__main__':
+
+    importerObj = Importer("Datasets/Preprocessed/DB-a")
+    super_matrix = importerObj.get_super_matrix()
+
+    dataLoader = DataLoader(super_matrix)
+    print(dataLoader.super_matrix.shape)
+
--- datahandler/Importer.py
+++ datahandler/Importer.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import numpy as np
 import os
 import sys
@@ -5,55 +6,138 @@ import scipy.io as sio
 import matplotlib.pyplot as plt
 import datetime
 
-
+#TODO: in the batch_loader, we cannot guarantee we always have a satisfying batch_number to not cause an index-error. If the batch-size is a divider of 1000, this shouldn't be a problem for now. Beware of empty batches in case there is no gesture for a specific person
 #TODO: implement Importer that lets you choose between: Intra-session, Cross-session, Cross-subject
 class Importer(object):
     """ All configurations assume that we currently import the CapgMyo dataset. Major work needs to be done if a different type of data is imported.
     """
-    """ Impoter is responsible for all preprocessing """
+    #TODO: Potentially, it could be wise to also have an axis for the frame-order - in case we need to
+    """ Impoter is responsible for all preprocessing
+        Will finally return an array, the 'super-matrix':
+           0  1  2  3  4  5  6  7  ...  n
+        -----------------------------------
+        |  s  g  t  d  d  d  d  ...  d
+        |  s  g  t  d  d  d  d  ...  d
+        |  s  g  t  d  d  d  d  ...  d
+        |  s  g  t  d  d  d  d  ...  d
+        |  s  g  t  d  d  d  d  ...  d
+        ...
+        Where:
+         s is the respective subject ID
+         g is the respective gesture ID
+         t is the respective trial
+         d is the respective frame (16 * 8)
+         potentially, we should also have a 'frame_number' id that determines the order of the frame within this gesture and session
+    """
 
     def __init__(self, datapath, verbose=True):
         """
         :param datapath: The root path that contains all the training data. The training data is assumed to be nested within another folder (2-level nesting)
         :return: An Importer object
         """
+        file_dirs = self.get_all_data_dirs(datapath, verbose=verbose)
+        file_list = self.get_data_from_datapath(file_dirs, verbose=verbose)
 
-        self.data_dirs = self.get_all_data_dirs(datapath, verbose=verbose)
-        self.data_list = self.get_data(self.data_dirs, verbose=verbose)
-
-        self.X = self.get_dict_property(self.data_list, "data")
-        self.y_tmp = self.get_dict_property(self.data_list, "gesture")
-
-        self.y_tmp = np.asarray(self.y_tmp)
-        self.X = np.asarray(self.X)
-
-        self.y_tmp = self.y_tmp.flatten()
-
+        #TODO: get rid of any unnecessary self. structures
+        #These lists should have the exact same order. They must! I hope I coded everything correctly, and python doesn't optimize away stuff.
+        self.data = self.get_dict_property(file_list, "data")
+        print("Data: ", np.asarray(self.data).shape)
+        self.data = np.reshape(self.data, (-1, 16 * 8))
+        print("Data: ", np.asarray(self.data).shape)
+        self.gesture = self.get_dict_property(file_list, "gesture")
         #TODO: change this into a hard-coded style, where additional classes are input depending on how many previous classes exist (a function that counts classes, and returns the number of different classes given a certain dataset
-        self.y_tmp[self.y_tmp == 100] = 9
-        self.y_tmp[self.y_tmp == 101] = 10
+        # self.gesture[self.gesture == 100] = 9
+        # self.gesture[self.gesture == 101] = 10
+        self.subject = self.get_dict_property(file_list, "subject")
+        self.trial = self.get_dict_property(file_list, "trial")
+
+        #to create the above given vector, we must repeat the first three values, if we want to treat the individual videos as frames
+        self.super_matrix = np.concatenate((self.gesture, self.subject, self.trial), axis=1)
+        print("Super Matrix 1: ", self.super_matrix.shape)
+        # now we repeat each of these values 1000 times, because we want to flatten out the videos to frames
+        # this results in about O(3%) of all data duplicated, which is accebtable given the convenience of the operations that will follow
+        self.super_matrix = np.repeat(self.super_matrix, 1000, axis=0)
+        print("Super Matrix 2: ", self.super_matrix.shape)
+        self.super_matrix = np.reshape(self.super_matrix, (-1, 3))
+        print("Super Matrix 3: ", self.super_matrix.shape)
+        # now we want to concatenate the data matrix next to this matrix. Before we can do that, we must flatten the video-format to individual frames (before: videos are samples; now: frames are samples)
+        self.super_matrix = np.concatenate((self.super_matrix, self.data), axis=1)
+        print("Super Matrix 4: ", self.super_matrix.shape)
 
         #Turn into one-hot
-        self.y = np.zeros((self.X.shape[0], 12))
-        self.y[np.arange(self.X.shape[0]), self.y_tmp] = 1
-
-        for i in range(self.X.shape[0]):
-            if np.sum(self.y[i]) != 1:
-                print("error!!")
-
-        self.X = np.reshape(self.X, (-1, 1000, 16, 8))
+        # self.y = np.zeros((self.X.shape[0], 12))
+        # self.y[np.arange(self.X.shape[0]), self.y_tmp] = 1
+        #
+        # for i in range(self.X.shape[0]):
+        #     if np.sum(self.y[i]) != 1:
+        #         print("error!!")
 
 
-    def get_trainingset(self):
+
+
+    #######################
+    # DATA OUTPUT FUNCTIONS
+    def get_super_matrix(self):
+        return self.super_matrix
+
+
+    def get_unclassified_dataset(self):
         """
-        :return: Returns the entire dataset X, and their respective one-hot labels
+        In this case, we don't care what subject and trial these gestures are from. As such, we can just spit out the entire dataset not sorted or split by any parameters.
+        :return: The tensor of emg video-frames, and their respective gesture label
         """
-        #TODO: change dimension of one-hot vector to adaptable size!
-        out_y = np.reshape(self.y, (-1, 1, 12))
-        out_y = np.repeat(out_y, 1000, axis=1)
-        return self.X, out_y
+        return self.X, self.y
+
+    def get_dataset_arr(self, mode):
+        """
+        Always outputs frames! No videos currently
+        :param mode:
+        :return:
+        """
+        if not (mode == "random" or mode == "random" or mode == "cross-subject"):
+            print("get_dataset_arr in Importer.py: Please select a valid mode! random, cross-session, cross-subject")
+            sys.exit(0)
+        if mode == "random":
+            pass
+        if mode == "cross-session":
+            pass
+        if mode == "cross-subject":
+            pass
 
 
+
+
+    def get_intra_session_dataset(self):
+        """
+        In this case, we must split the dataset by the ses
+        :return:
+        """
+        pass
+
+
+    def get_cross_session_dataset(self):
+        """
+        In this case, we care about which emg video-frames was create by which session.
+        :return: Return an array of tensors. Each tensor consists of the emg video-frames and the gesture label. Every two array-elements contains data from different sessions.
+        """
+        pass
+
+
+    def get_cross_subject_dataset(self):
+        """
+        In this case, we care about which emg video-frames was created by whom
+        :return: Return an array of tensors. Each tensor consists of the emg video-frames and the gesture label. Every two array-elements contains data from different subjects.
+        """
+        pass
+
+
+
+
+
+
+
+    #######################
+    # DIRECTORY FUNCTIONS
     def get_all_data_dirs(self, directory, verbose=False):
         """
         :param directory: The directory in which all the datasets are filed in.
@@ -74,7 +158,7 @@ class Importer(object):
         return out
 
 
-    def get_data(self, datapath, verbose=False):
+    def get_data_from_datapath(self, datapath, verbose=False):
         """
         :param datapath: An single datapath, or an array of datapaths.
         :param verbose:
@@ -102,6 +186,9 @@ class Importer(object):
         return out
 
 
+
+    #######################
+    # HELPER FUNCTIONS
     def get_dict_property(self, data_arr, property):
         """
         :param data_arr: An array of dictionaries.
@@ -115,7 +202,19 @@ class Importer(object):
 
         return out
 
+    def pop_dict_property(self, data_arr, property):
+        out = []
+        for data in data_arr:
+            tmp = data
+            tmp.pop(property, None)
+            out.append( tmp )
 
+        return out
+
+
+
+    #######################
+    # DATA SAMPLE FUNCTIONS
     def sample_data(self):
         """
         Shows you video animation of a 1000 frame data-sample.
@@ -181,15 +280,8 @@ class Importer(object):
 
 def main():
     importerObj = Importer("Datasets/Preprocessed/DB-a")
-    X = importerObj.get_X()
-    importerObj.sample_data()
 
-    print(X.shape)
-    print(X)
-
-    y = importerObj.get_y()
-    print(y.shape)
-    print(y)
+    X, y = importerObj.get_trainingset()
 
 if __name__ == "__main__":
     main()
