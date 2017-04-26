Logging within the cross-subject part was implemented. A bug where a wrong value was divided from the difference_vector (accuracy voting) was debugged.

crossvalidate.py
@@ -4,7 +4,31 @@ from datahandler.DataLoader import *
 from metrics.AccuracyTester import *
 from model.BuildGraph import *
 from train import *
+import logging
+logging = logging.getLogger(__name__)
 
+import os
+import json
+import logging.config
+
+def setup_logging(
+    default_path='logging.json',
+    default_level=logging.INFO,
+    env_key='LOG_CFG'
+):
+    """Setup logging configuration
+
+    """
+    path = default_path
+    value = os.getenv(env_key, None)
+    if value:
+        path = value
+    if os.path.exists(path):
+        with open(path, 'rt') as f:
+            config = json.load(f)
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=default_level)
 
 #TODO: implement a logging system
 #TODO: go through code for: 1. any sytnax mistakes / improvements 2. any sematic mistakes / improvements 3. and structural mistakes / improvements (encapsulation etc.)
@@ -182,28 +206,39 @@ def crossvalidate_crosssession(parameter, dataLoader):
 
 def crossvalidate_subjects(parameter, dataLoader, is_voting):
 
+    logging.debug("Entering cross_validate_subjects function")
+
     cross_subject_dataset = dataLoader.get_cross_subject_dataset()
 
-    print("Data loader: ", dataLoader.sm.shape)
-    print("Cross subject elements: ", len(cross_subject_dataset))
+    logging.debug("Data loader's super_matrix has shape: {}".format(dataLoader.sm.shape))
+    logging.debug("The number of different subjects: {}".format(len(cross_subject_dataset)))
 
+    logging.debug("Initializing tensorflow graph")
     ## Initializing TensorFlow Graph
-    tf.global_variables_initializer()
+    tf.global_variables_initializer() #TODO: change this to initialize_all_variables! or vice verca
     W, b, model_dict = init_graph()
+
+    logging.debug("tensorflow graph initialized successfully")
 
     ## Cross Validate
     accuracy_list = []
 
     for i in range(len(cross_subject_dataset)):
         #tf.reset_default_graph() #TODO: Need to reinitialize weights after each run!! Opening and closing a session each time is a hacky solution!
+        logging.info("Cross-validating by leaving subject number {} out".format(i))
         with tf.Session() as sess:
 
+            logging.debug("Re-initializing graph")
             init = tf.initialize_all_variables()
             sess.run(init)
+            logging.debug("Graph re-initialized successfully")
 
             X_cv, y_cv = cross_subject_dataset[i]
             X_cv = np.reshape(X_cv, (-1, 16, 8))
             y_cv = np.concatenate((y_cv, np.zeros((y_cv.shape[0], 2))), axis=1) #TODO: very hacky solution. Must change this value in the model later!!!! Create a variable within the model for this!
+
+            logging.debug("X_cv has shape: {}".format(X_cv.shape))
+            logging.debug("y_cv has shape: {}".format(X_cv.shape))
 
             train_set = [cross_subject_dataset[j] for j in range(len(cross_subject_dataset)) if i != j] #One could also just flatten the list, and every odd one is X, every even one is y. I just didn't want to introduce futher libraries.
 
@@ -212,9 +247,16 @@ def crossvalidate_subjects(parameter, dataLoader, is_voting):
             y_train = np.concatenate([y for x, y in train_set], axis=0)
             y_train = np.concatenate((y_train, np.zeros((y_train.shape[0], 2))), axis=1) #TODO: very hacky solution. Must change this value in the model later!!!! Create a variable within the model for this!
 
-            X_train = X_train[:1000, :, :]
-            y_train = y_train[:1000, :]
+            logging.debug("X_train has shape: {}".format(X_train.shape))
+            logging.debug("y_train has shape: {}".format(X_train.shape))
 
+            # logging.warning("Using only the first 1000 elements!")
+            # X_train = X_train[:1000, :, :]
+            # y_train = y_train[:1000, :]
+
+            #TODO: save model, and allow option to retrieve saved model (simple if-statement); we can also just say we want a saver object with a certain path. Might need to change the parameter['SAVE_DIR'] option
+
+            logging.debug("Entering train function")
             train(
                         sess=sess,
                         parameter=parameter,
@@ -227,7 +269,7 @@ def crossvalidate_subjects(parameter, dataLoader, is_voting):
             accuracy = 0
 
             if is_voting:
-                print("Entering voting function")
+                logging.debug("Entering voting function")
                 accuracy = voting(
                             sess=sess,
                             model_dict=model_dict,
@@ -238,6 +280,7 @@ def crossvalidate_subjects(parameter, dataLoader, is_voting):
                             verbose=False
                 )
             else:
+                logging.debug("Entering vanilla accuracy tester function")
                 accuracy = test_accuracy(
                             sess=sess,
                             model_dict=model_dict,
@@ -246,24 +289,35 @@ def crossvalidate_subjects(parameter, dataLoader, is_voting):
                             y=y_cv
                 )
 
+            logging.warning('Accuracy of leave-out subject is {:.3f}'.format(accuracy*100))  # will not print anything
+
             accuracy_list.append(accuracy)
 
     total_accuracy = float(sum(accuracy_list))/len(cross_subject_dataset) * 100
-    print("Accuracy of the current model on cross-subject is: {:.3f}%".format(total_accuracy))
+    logging.warning("Accuracy of the current model on cross-subject is: {:.3f} percent ".format(total_accuracy))
+    print("Accuracy of the current model on cross-subject is: {:.3f} percent ".format(total_accuracy))
 
     return accuracy_list
 
 
-def main(parameter, mode, is_voting):
+def main(parameter, mode, is_voting, path):
     #Basically, we always perform 'leave-one-out' cross validation
 
     ################################
     # Importing all data
     ################################
-    importerObj = Importer("Datasets/Preprocessed/DB-a")
+    logging.debug("Using the Importer object on the path: {}".format(path))
+    logging.debug("Using the parameter: {}".format(parameter))
+    logging.info("Testing on: {}".format(mode))
+    logging.debug("Using voting: {}".format(is_voting))
+
+    importerObj = Importer(path)
     super_matrix = importerObj.get_super_matrix()
 
     dataLoader = DataLoader(super_matrix)
+
+    logging.debug("The imported super_matrix has shape: {}".format(super_matrix.shape))
+    logging.debug("dataLoader has type: {}".format(type(dataLoader)))
 
     #TODO: create a logging style, such that multiple modes at once can be selected aswell
     if mode=="cross-session":
@@ -273,7 +327,7 @@ def main(parameter, mode, is_voting):
     elif mode=="cross-subject":
         crossvalidate_subjects(parameter, dataLoader, is_voting)
     else:
-        print("No mode specified!")
+        logging.error("No mode (cross-session, cross-subject, intra-session) was specified!")
         sys.exit(11)
 
 
@@ -304,7 +358,11 @@ if __name__ == '__main__':
             'LEARNING_RATE': 0.1
     }
 
+    setup_logging()
 
-    main(test_parameter, mode="cross-subject", is_voting=True)
+
+    path = "Datasets/Preprocessed/DB-a"
+
+    main(paper_parameter, mode="cross-subject", is_voting=True, path=path)
 
 #TODO: If we want pre-training, we must select all subjects for cross-session; pick all but one for pre-training, and apply the not-selected set as done above with the subjects

metrics/AccuracyTester.py
@@ -9,38 +9,45 @@ from datahandler.Importer import *
 from datahandler.DataLoader import *
 import tensorflow as tf
 
+import logging
+logging = logging.getLogger(__name__)
+
 
 
 #TODO: implement a 'sliding' windows over the framesize. Currently it works only to vote over frames of 1000 frames!
+#TODO: remove verbosity
 def voting(sess, model_dict, parameter, X, y, framesize=1000, verbose=False):
-    batchLoder = BatchLoader(X, y, framesize, shuffle=False)
+    logging.debug("Entering voting function")
 
     if 1000 % framesize != 0:
-        print("Framesize not divisible by 1000! Currently is: ", framesize)
+        logging.error("Framesize not divisible by 1000! Currently is: {}".format(framesize))
         sys.exit(11)
 
-
-    #TODO: split by framsizes, or 1000's, such that we can automatically input multiple datasamples
-    split_indices = [i for i in range(framesize, X.shape[0], framesize)]
-
+    batchLoder = BatchLoader(X, y, framesize, shuffle=False)
     predict_list = []
     actual_list = []
-
     epoch_done = False
 
+    #TODO: what exactly do we do here? We vote over multiple frames to get a final, single frame
     while not epoch_done:
+        logging.debug("Starting next voting session")
 
         X_batch, y_batch, epoch_done = batchLoder.load_batch()
 
+        logging.debug("X_batch has shape: {}".format(X_batch.shape))
+        logging.debug("y_batch has shape: {}".format(y_batch.shape))
+
         if not np.array_equal(y_batch, np.full(y_batch.shape, y_batch[0])): #i think it will fail on this part due to the dimensions!
             print("y_batch should be one-dimensional, but is of size: ", y_batch.shape)
-            print("Not all samples are from the same sample! No majority voting possible!")
+            logging.error("y_batch should be one-dimensional, but is of size: ".format(y_batch.shape))
+            logging.error("Not all samples are from the same sample! No majority voting possible!")
             sys.exit(11)
 
-
+        #Do i need this?
         # X_tmp = np.reshape(X_batches[i], (-1, 16, 8))
         # y_tmp = y_batches[i]
 
+        logging.debug("Entering sess.run from within the voting function")
         logit = sess.run(
                             #Describe what we want out of the model
                             [
@@ -54,42 +61,32 @@ def voting(sess, model_dict, parameter, X, y, framesize=1000, verbose=False):
                                 model_dict['isTraining']: False
                             }
         )
+        logging.debug("Finished sess.run from within the voting function")
 
         logit = logit[0]
 
-
-
         predict = np.argmax(logit, axis=1) #should take the maximum value out of all values!
-        if verbose:
-            print("predict is: ", predict)
-            print("predict shape is: ", predict.shape)
+        logging.debug("Predict has shape: {} after choosing the maximum value (argmaxing)".format(predict.shape))
 
         predict = np.bincount(predict)
         predict = np.argmax(predict)
-
-
+        logging.debug("Predict has shape: {} after having majority-voted (taking the most common occurence)".format(predict.shape))
 
         actual = np.bincount(np.argmax(y_batch, axis=1)) #should return a random element which is equivalent to all elements #Do i need to change this / process this?
         actual = np.argmax(actual)
-
-        if verbose:
-            print("actual is: ", actual)
-            print("actual shape is: ", actual.shape)
-
+        logging.debug("Actual has shape: {} after having majority-voted (taking the most common occurence)".format(actual.shape))
 
         predict_list.append(predict)
         actual_list.append(actual)
 
-    # print("Predict is: ", predict_list)
-
-    difference = [1 if pred == act else 0 for pred, act in zip(predict_list, actual_list)]
-    accuracy = (np.sum(difference) / float(X_batch.shape[0]))
+    difference_vector = [1 if pred == act else 0 for pred, act in zip(predict_list, actual_list)]
+    accuracy = (np.sum(difference_vector) / float(len(predict_list)))
 
     ##############################
     # Confusion matrix
     ##############################
-    print("Accuracy is: {:.3f}%".format(accuracy*100))
-    print("Random baseline: {:.3f}%".format(1./32*100))
+    logging.warning("Accuracy is: {:.3f}%".format(accuracy*100))
+    logging.warning("Random baseline: {:.3f}%".format(1./32*100))
 
     return accuracy
 

train.py
@@ -4,6 +4,8 @@ from datahandler.Importer import *
 from datahandler.BatchLoader import BatchLoader
 from datahandler.Saver import *
 
+import logging
+logging = logging.getLogger(__name__)
 
 # parameter = {
 #            'NUM_EPOCHS': 1,
@@ -29,6 +31,8 @@ def train(sess, parameter, model_dict, X, y, saverObj):
 
     for epoch in xrange(parameter['NUM_EPOCHS']):
 
+        logging.info("Staring Epoch: {}".format(epoch))
+
         loss_list = [] #we want loss_list to be re-initialized at each iteration
 
         start_time = datetime.datetime.now()
@@ -51,16 +55,17 @@ def train(sess, parameter, model_dict, X, y, saverObj):
 
         loss_list.extend(loss)
 
+        logging.debug("Epoch took us: {}".format(total_time))
+        logging.debug("Epoch had loss: {}".format(loss))
+        logging.debug("Average loss of epochs so far: {} (this value should decrease)".format(np.sum(loss_list) / parameter['NUM_EPOCHS']))
+
         percentage = float(epoch) / parameter['NUM_EPOCHS']
 
         if saverObj is not None:
             saverObj.save_session(sess, parameter['SAVE_DIR'])
 
         print("Progress: {0:.3f}%%".format(percentage * 100))
-        print("EST. time per episode: " + str(total_time))
-        print("Epochs left: {0:d}".format(parameter['NUM_EPOCHS'] - epoch))
-        print("Average loss of current epoch: " + str(np.sum(loss_list) / parameter['NUM_EPOCHS']))
-        print("")
+        print("EST. time left: " + str(total_time)) #TODO: multiply this by parameter['NUM_EPOCHS'] to get estimated time left!
 
     return loss_list
 
@@ -84,6 +89,11 @@ def run_epoch(sess, cur_epoch, parameter, model_dict, X, y, saverObj):
 
         X_batch, y_batch, epoch_done = batchLoader.load_batch()  # TODO: this runs one time too much. Create an initializer and move it to the end, or create a while True with a break condition
 
+        logging.debug("X_batch has size: {}".format(X_batch.shape))
+        logging.debug("y_batch has size: {}".format(y_batch.shape))
+
+
+        logging.debug("Going into sess.run within the run_epoch function")
         loss, predict, _, _ = sess.run(
             # Describe what we want out of the model
             [
@@ -102,15 +112,16 @@ def run_epoch(sess, cur_epoch, parameter, model_dict, X, y, saverObj):
                 model_dict['isTraining']: True
             }
         )
+        logging.debug("Having left the sess.run within the run_epoch function")
 
         if save_iter % parameter['SAVE_EVERY'] == 0:
             if saverObj is not None:
+                logging.debug("Saving file to disk")
                 saverObj.save_session(sess, parameter['SAVE_DIR'], tf.train.global_step(sess,
                                                                                         global_step_tensor=model_dict[
                                                                                             'globalStepTensor']))  # step in terms of batches #cur_epoch * batchLoader.number_of_batches + batchLoader.batch_counter
-            print("\nStep progress: ", 100. * batchLoader.batch_counter / batchLoader.number_of_batches)
-            print("Training Loss: ", np.sum(loss) / batchLoader.batch_size)
-            print("Epoch Progression: ", cur_epoch / float(parameter['NUM_EPOCHS']))
+                logging.debug("File-save successful")
+            logging.debug("Step progress: {:3f} in epoch {}".format(100. * batchLoader.batch_counter / batchLoader.number_of_batches, cur_epoch))
 
         save_iter += 1
 